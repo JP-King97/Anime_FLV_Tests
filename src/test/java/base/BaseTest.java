bd22aef44c9ad992baf6a4dd7db79d83ff6fc3c0
JPKingtero <JPQM4897@gmail.com>
2023-07-08 18:03:50 -0500

    feat(BaseTest): the base test was added and two methods, one for set up the driver and one for shut down the driver.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,29 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class BaseTest {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOpt = new ChromeOptions();
+        chromeOpt.addArguments("--no sandbox");
+        chromeOpt.addArguments("--disable-dev-shm-usage");
+        chromeOpt.addArguments("--window-size=1920,1080");
+        chromeOpt.addArguments("--headless");
+        driver = new ChromeDriver(chromeOpt);
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
+    }
 }
