63c57a248dc1807eb2585ae624472d9419c570a3
JPKingtero <JPQM4897@gmail.com>
2023-08-19 12:49:29 -0500

    fix(Jenkinsfile): the VNC recorder was modified 31

--- Jenkinsfile
+++ Jenkinsfile
@@ -14,67 +14,11 @@ pipeline{
             }
         }
 
-        stage('Download and Install Xvnc') {
-            steps {
-                script {
-                    // Define the URL to download the Xvnc installation package
-                    def xvncUrl = 'https://example.com/path/to/xvnc_installer.exe'  // Replace with the actual download URL
-
-                    // Define the installation directory
-                    def installDir = 'C:\\Xvnc'  // Replace with your desired installation directory
-
-                    // Create the installation directory if it doesn't exist
-                    bat "mkdir -p ${installDir}"
-
-                    // Download Xvnc installer
-                    bat "curl -o ${installDir}\\xvnc_installer.exe ${xvncUrl}"
-
-                    // Install Xvnc silently (modify the installer command as needed)
-                    bat "${installDir}\\xvnc_installer.exe /S"  // /S for silent installation
-
-                    // Optionally, add Xvnc to the system PATH
-                    bat 'setx PATH "%PATH%;C:\\Xvnc"'
-
-                    // Verify Xvnc installation (you can modify this as needed)
-                    bat "C:\\Xvnc\\xvnc.exe --help"  // Example: Check if Xvnc responds to --help
-
-                    // Clean up the installer if needed
-                    bat "del /q ${installDir}\\xvnc_installer.exe"
-                }
-            }
-        }
-
-        stage('Start VNC session'){
-            steps{
-                script{
-                    bat 'Xvnc :1 -screen 0 1024x768x16 &'
-                    bat 'export DISPLAY=:1'
-                    bat 'taskkill /F /IM Xvnc.exe'
-                }
-            }
-        }
-
-        stage('Start VNC Recording'){
-            steps{
-                vncRecorder(
-                    server: 'Kingtero97',
-                    port: 5900,
-                    password: 'J3141592'
-                )
-            }
-        }
-
         stage('Test'){
             steps{
                 script{
                     bat "mvn clean test"
                 }
-            }
-        }
-
-        stage('Stop VNC Recording'){
-            steps{
-                vncRecorderStop()
             }
         }
 
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,5 +1,8 @@
 package base;
 
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+import com.github.sarxos.webcam.WebcamStreamer;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,7 +15,9 @@ import org.testng.annotations.Test;
 import pages.HomePage;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 public class BaseTest {
     public WebDriver driver;
@@ -23,12 +28,33 @@ public class BaseTest {
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions chromeOpt = new ChromeOptions();
+
+        chromeOpt.addArguments("--start-maximized");
+        chromeOpt.addArguments("--disable-infobars");
+        chromeOpt.addArguments("--disable-extensions");
+
+        chromeOpt.addArguments("--disable-gpu");
+        chromeOpt.addArguments("--disable-software-rasterizer");
+
         chromeOpt.addArguments("--no-sandbox");
         chromeOpt.addArguments("--disable-dev-shm-usage");
         chromeOpt.addArguments("--window-size=1920,1080");
         chromeOpt.addArguments("--headless");
         driver = new ChromeDriver(chromeOpt);
         wait = new WebDriverWait(driver, Duration.ofSeconds(15000));
+        //Webcam webcam = Webcam.getDefault();
+        //webcam.setViewSize(WebcamResolution.VGA.getSize()); // Set video resolution
+        //webcam.open();
+//
+        //// Specify the output directory and file format (e.g., AVI)
+        //String outputDirectory = "path_to_output_directory";
+        //String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        //String outputFileName = "video_" + timestamp + ".avi";
+        //File outputFilePath = new File(outputDirectory, outputFileName);
+//
+        //// Initialize the video streamer
+        //WebcamStreamer streamer = new WebcamStreamer(webcam, outputFilePath, Webcam);
+
         driver.get("https://www3.animeflv.net/");
         homePg = new HomePage(driver,wait);
         driver.manage().window().maximize();
@@ -36,6 +62,9 @@ public class BaseTest {
 
     @AfterTest
     public void tearDown(){
+     //  Webcam webcam = Webcam.getDefault();
+     //  webcam.close();
         driver.quit();
     }
+
 }
